449cfa40d025af7aab0c6ea64d495fce607a2ee8
Igor Tkachuk <I.Tka4yk@gmail.com>
2022-07-10 21:29:41 +0300

    fix: app.go

--- cmd/app.go
+++ cmd/app.go
@@ -1,14 +1,13 @@
 package main
 
-import (
-	"fmt"
-	"gitlab.com/wshaman/hw-concurrency/lib/batch"
-)
+import "fmt"
 
 func main() {
-	ul := batch.GetBatch(40, 4)
+	//ul := batch.GetBatch(40, 4)
+	//
+	//for _, u := range ul {
+	//	fmt.Println(u.ID)
+	//}
 
-	for _, u := range ul {
-		fmt.Println(u.ID)
-	}
+	fmt.Println("Hello world!")
 }
